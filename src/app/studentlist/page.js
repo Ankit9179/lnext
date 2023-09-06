01962f26b233df397ab4e9d02e0d7128443a135c
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-09-06 10:24:47 +0530

    updata readme file

--- src/app/studentlist/page.js
+++ src/app/studentlist/page.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
 
 export default function StudentList() {
+  const arr = [ankit, iron, jon, nick];
   return (
     <div className="ml-6">
       <h1 className="m-4">student list</h1>
       <ul className="text-blue-700">
-        <li>
-          <Link href="/studentlist/ankit">ankit</Link>
-        </li>
-        <li>
+        {arr.map((item) => {
+          return (
+            <li>
+              <Link href="/studentlist/ankit">{item}</Link>
+            </li>
+          );
+        })}
+        {/* <li>
           <Link href="/studentlist/iron">iron</Link>
         </li>
         <li>
@@ -16,7 +21,7 @@ export default function StudentList() {
         </li>
         <li>
           <Link href="/studentlist/nick">nick</Link>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
