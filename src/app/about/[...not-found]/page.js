a4e3a036c77c126b39a60e7d2b4ebca0e07110e6
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-09-06 11:36:11 +0530

    my task in 404 page

--- src/app/about/[...not-found]/page.js
+++ src/app/about/[...not-found]/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function AboutNotFound() {
   return (
     <div className="flex justify-center flex-col  align-middle h-[500px]">
       <h1 className="mx-auto"> this about page not found</h1>
+      <Link href="/about" className="text-blue-300 text-center">
+        click here to about page
+      </Link>
     </div>
   );
 }
